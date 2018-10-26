36ec6e66e5c73609d64779a830158652062cb57e
Jonathan Samines <jn.samines@gmail.com>
2018-10-26 08:14:10 -0600

    [creacion-factura] Cargar tipos de impuestos asociados a un producto

--- seeds/afiliacion-iva.js
+++ seeds/afiliacion-iva.js
@@ -2,7 +2,8 @@
 
 exports.seed = async (knex, Promise) => {
 
-
+    await knex('impuesto_tipo_producto').del();
+    await knex('tipo_producto').del();
     await knex('unidad_gravable').del();
     await knex('impuesto_tipo_documento').del();
     await knex('tipo_frase_tipo_documento').del();
@@ -17,6 +18,7 @@ exports.seed = async (knex, Promise) => {
     await knex('tipo_frase').insert(require('./fixtures/tipo-frase'));
     await knex('tipo_documento').insert(require('./fixtures/tipos-documentos'));
     await knex('afiliacion_iva').insert(require('./fixtures/afiliacion-iva'));
+    await knex('tipo_producto').insert(require('./fixtures/tipos-producto'));
     await knex('frase').insert(require('./fixtures/frases'));
     await knex('afiliacion_iva_tipo_documento').insert(require('./fixtures/afiliacion-iva-tipo-documento.json'));
     await knex('tipo_frase_tipo_documento').insert(require('./fixtures/tipo-frase-tipo-documento'));
@@ -24,4 +26,5 @@ exports.seed = async (knex, Promise) => {
     await knex('moneda').insert(require('./fixtures/monedas'));
     await knex('unidad_gravable').insert(require('./fixtures/unidad-gravable'));
     await knex('impuesto_tipo_documento').insert(require('./fixtures/impuesto-tipo-documento'));
+    await knex('impuesto_tipo_producto').insert(require('./fixtures/impuesto-tipo-producto'));
 };
