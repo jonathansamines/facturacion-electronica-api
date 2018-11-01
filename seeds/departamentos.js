61bd0777bb453dd0f9fcd2b5fe8447da073abc1a
Jonathan Samines <jn.samines@gmail.com>
2018-10-31 20:00:17 -0600

    [creacion-factura] Agregar soporte para Paises. Creación inicial del modelo de factura electrónica

--- seeds/departamentos.js
+++ seeds/departamentos.js
@@ -4,7 +4,9 @@ exports.seed = async (knex, Promise) => {
 
     await knex('municipio').del();
     await knex('departamento').del();
+    await knex('pais').del();
 
+    await knex('pais').insert(require('./fixtures/paises'));
     await knex('departamento').insert(require('./fixtures/departamentos'));
     await knex('municipio').insert(require('./fixtures/municipios'));
 };
